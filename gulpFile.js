e4bbc12d4b35ada291af4d627819fe8299aaa9c2
Attila Gal <attila.gal@gmail.com>
2016-12-03 21:07:10 +0100

    added gulpFile with typescript build

---
+++ gulpFile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var angularFilesort = require('gulp-angular-filesort');
+
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('move-files', function(){
+  return gulp
+    .src([
+      './app/**/*.html',
+      './app/**/*.css',
+      './app/**/*.jpeg',
+      './app/**/*.jpg',
+      './app/**/*.png',
+      './app/**/*.gif',
+      './app/**/*.json'
+    ])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('typescript', function(){
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inject', ['move-files', 'typescript'], function(){
+  return gulp
+    .src('./dist/index.html')
+    .pipe(inject(
+      gulp
+        .src('./dist/**/*.js')
+        .pipe(angularFilesort())
+      ))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['move-files', 'typescript', 'inject']);
